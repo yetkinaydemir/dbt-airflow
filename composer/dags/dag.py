f82c3a1a64c9750c14a7769f2a5430d647cb4a2d
yetkinaydemir <yetkinaydemirr@gmail.com>
2022-07-04 14:09:17 +0300

    .

--- composer/dags/dag.py
+++ composer/dags/dag.py
@@ -23,7 +23,7 @@ with DAG(
     task_2 = DbtSnapshotOperator(
         task_id = 'dim_billing_address_snapshot',
         select = 'dim_billing_address',
-        dir='https://console.cloud.google.com/storage/browser/europe-west1-dbt-build-4eb5c497-bucket/dags/dbt',
+        dir='https://console.cloud.google.com/storage/browser/europe-west1-dbt-build-4eb5c497-bucket/dags/dbt/',
         dag=dag
     )
 
